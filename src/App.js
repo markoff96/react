15c9dced8fa17fd73e41a6355b9aa57f3ef8468d
Kirill <punisher543@mail.com>
2023-02-15 17:06:04 +0300

    Adding a paragraph

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
+import ParModule from './components/ParModule'
 
 function App() {
-  return <div className="App"></div>
-}
+  return (
+    <div className='App'>
+      <MyComponent />
+      <ParModule />
 
+    </div>
+  )
+}
 export default App
